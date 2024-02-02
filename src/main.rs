42ea5f9e2626c9b7c9e509958599c5b6bc5a73c8
PurplProto <pairedprototype@hotmail.com>
2024-02-02 18:51:50 +0000

    Introduced Clap for a nice interface

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use clap::{ArgAction, Args, Parser, Subcommand};
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
+use std::process::exit;
 
 mod hid_ops;
 mod key_map;
@@ -7,20 +9,75 @@ mod pin_list_4_digit;
 
 static KEYBOARD_DEVICE: &str = "/dev/hidg0";
 
-fn main() {
-    SimpleLogger::new()
-        .with_level(LevelFilter::Info)
-        .env()
-        .init()
-        .unwrap();
-    log::info!("Starting app...");
+#[derive(Parser)]
+#[command(author, version, about, long_about = None, arg_required_else_help = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
 
-    log::info!("Writing to device file...");
-    let result = hid_ops::write_to_device_file(KEYBOARD_DEVICE, "Hello World");
-    match result {
-        Ok(_) => log::info!("Successfully wrote to device file"),
-        Err(_) => log::error!("Failed to write to device file"),
+    /// Optional device file to use. Defaults to: /dev/hidg0
+    #[arg(short, long)]
+    device: Option<String>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = ArgAction::Count)]
+    verbose: u8,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// starts brute force attack
+    Start,
+    /// resumes brute force attack
+    Resume(ResumeArgs),
+}
+
+#[derive(Args)]
+struct ResumeArgs {
+    /// pin to resume from
+    pin: String,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let mut logger = SimpleLogger::new();
+
+    logger = match cli.verbose {
+        0 => logger.with_level(LevelFilter::Info),
+        1 => logger.with_level(LevelFilter::Debug),
+        _ => logger.with_level(LevelFilter::Trace),
+    };
+
+    logger.env().init().unwrap();
+
+    log::debug!("Starting app...");
+
+    match cli.command {
+        Some(Commands::Start) => start(),
+        Some(Commands::Resume(args)) => resume(args),
+        None => {
+            log::error!("No command provided. Use --help for usage information.");
+            exit(1);
+        }
     }
 
-    log::info!("Finished app...");
+    log::debug!("Finished app...");
+}
+
+fn start() {
+    log::info!("Starting brute force attack...");
+    let result = hid_ops::write_to_device_file(KEYBOARD_DEVICE, "Hello World");
+    match result {
+        Ok(_) => log::info!("Brute force attack complete"),
+        Err(e) => log::error!("Failed to start brute force attack: {}", e),
+    }
+}
+
+fn resume(args: ResumeArgs) {
+    log::info!("Resuming brute force attack from pin: {}", args.pin);
+    let result = hid_ops::write_to_device_file(KEYBOARD_DEVICE, "Hello World");
+    match result {
+        Ok(_) => log::info!("Brute force attack complete"),
+        Err(e) => log::error!("Failed to start brute force attack: {}", e),
+    }
 }
