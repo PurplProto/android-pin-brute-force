9029b5c01c3f4eff056a3ddfbf4c03c2ae4e0e95
PurplProto <pairedprototype@hotmail.com>
2024-02-23 00:42:47 +0000

    Add script to create new tag (GitHub release)

---
+++ scripts/create-new-gh-release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "[sudo] Ensuring dependencies are installed."
+
+sudo snap install yq
+
+echo "pushing tag to trigger a release"
+
+version_tag="v$(yq e '.package.versions' Cargo.toml)"
+
+git tag -a "$version_tag" -m "$version_tag"
+git push origin "$version_tag"
