f8340e190d2e315434e438576838389b2b72e14f
PurplProto <pairedprototype@hotmail.com>
2024-02-23 00:49:31 +0000

    Fixed typo

--- scripts/create-new-gh-release.sh
+++ scripts/create-new-gh-release.sh
@@ -8,7 +8,7 @@ sudo snap install yq
 
 echo "pushing tag to trigger a release"
 
-version_tag="v$(yq e '.package.versions' Cargo.toml)"
+version_tag="v$(yq e '.package.version' Cargo.toml)"
 
 git tag -a "$version_tag" -m "$version_tag"
 git push origin "$version_tag"
